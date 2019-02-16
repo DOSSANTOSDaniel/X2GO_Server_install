d1f12b430dd5fdb23d857ab2ad0fb5a959a87eb5
DOS SANTOS <daniel.massy91@gmail.com>
2019-02-16 23:11:09 +0100

    Update inst_x2go.sh

--- inst_x2go.sh
+++ inst_x2go.sh
@@ -2,6 +2,7 @@
 #Auteurs:
 #"HALHOUL Billel" halhoul.billel.pro@gmail.com
 #"DOS SANTOS Daniel" daniel.massy91@gmail.com
+#Source: https://wiki.x2go.org/doku.php/doc:installation:x2goserver
 
 echo  "Installation en cours"
 apt-key adv --recv-keys --keyserver keys.gnupg.net E1F958385BFE2B6E
@@ -11,5 +12,6 @@ apt-get install x2goserver x2goserver-xsession -y
 apt-get install xfce4 iceweasel -y
 echo -e "\n Installation terminé, si le service X2GO n'a pas démarré taper service x2goserver start \n "
 echo -e " Tests pour vérifier le service X2GO \n"
-ps aux | grep x2go
+#ps aux | grep x2go
+service --status-all | grep x2go
 
