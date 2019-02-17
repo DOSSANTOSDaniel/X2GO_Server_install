fd66bda5695debab5cdf9efde886827c8ae5e630
DOS SANTOS <daniel.massy91@gmail.com>
2019-02-17 01:07:01 +0100

    Update inst_x2go.sh

--- inst_x2go.sh
+++ inst_x2go.sh
@@ -3,17 +3,21 @@
 #"HALHOUL Billel" halhoul.billel.pro@gmail.com
 #"DOS SANTOS Daniel" daniel.massy91@gmail.com
 #Source: https://wiki.x2go.org/doku.php/doc:installation:x2goserver
-
+clear
 echo -e "\n Début de l'installation \n"
 apt-key adv --recv-keys --keyserver keys.gnupg.net E1F958385BFE2B6E
-echo "deb http://packages.x2go.org/debian jessie main" > /etc/apt/sources.list.d/x2go.list
-echo "deb http://archives.packages.x2go.org/debian jessie extras main" >> /etc/apt/sources.list.d/x2go.list
-echo "deb-src http://archives.packages.x2go.org/debian jessie extras main" >> /etc/apt/sources.list.d/x2go.list
+echo "deb http://packages.x2go.org/debian/ jessie main contrib" > /etc/apt/sources.list.d/x2go.list
 apt-get update -y
+sleep 1
+clear
 apt-get install x2go-keyring -y
 apt-get update -y
+sleep 1
+clear
 apt-get install x2goserver x2goserver-xsession -y
 apt-get install xfce4 iceweasel -y
+sleep 1
+clear
 service x2goserver status
 echo -e "\n Installation terminé, si le service X2GO n'a pas démarré taper 'service x2goserver start' \n"
 echo -e " Service X2GO server démarré! \n"
