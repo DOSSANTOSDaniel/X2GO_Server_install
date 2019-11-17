fc64c40894a52e300089f5f39bc7c202ce09bb7b
DOS SANTOS <danielitto91@gmail.com>
2019-11-17 02:03:32 +0100

    Update inst_x2go.sh

--- inst_x2go.sh
+++ inst_x2go.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#Script permettant l'installation de X2GO serveur et de l'environnement de bureau XFCE ainsi que le navigateur Iceweasel.
+#Script permettant l'installation de:
+# X2GO serveur
+# L'environnement de bureau XFCE4
+# Le navigateur Iceweasel
+# Le terminal xfce4-terminal
+#
 #"HALHOUL Billel" halhoul.billel.pro@gmail.com
 #"DOS SANTOS Daniel" daniel.massy91@gmail.com
 #Source: https://wiki.x2go.org/doku.php/doc:installation:x2goserver
@@ -7,6 +12,8 @@ clear
 echo -e "\n Début de l'installation \n"
 apt-get install dirmngr -y
 clear
+while [ : ]
+do
 echo ''
 echo "Quelle est votre version de Debian ?"	
 echo '------------------------------------'
@@ -25,13 +32,15 @@ fi
 case $ch in
 1) echo "deb http://packages.x2go.org/debian stretch extras main" > /etc/apt/sources.list.d/x2go.list
 echo "deb-src http://packages.x2go.org/debian stretch extras main" >> /etc/apt/sources.list.d/x2go.list
+break
 ;;
 2) echo "deb http://packages.x2go.org/debian/ jessie main contrib" > /etc/apt/sources.list.d/x2go.list
 echo "deb-src http://packages.x2go.org/debian jessie main contrib" >> /etc/apt/sources.list.d/x2go.list
+break
 ;;
 *) error "choix non proposé !"
 esac
-
+done
 apt-get update
 sleep 1
 clear
@@ -40,8 +49,11 @@ apt-key adv --recv-keys --keyserver keys.gnupg.net E1F958385BFE2B6E
 apt-get update
 sleep 1
 clear
-apt-get install x2goserver x2goserver-xsession -y
-apt-get install xfce4 iceweasel -y
+apt-get install x2goserver -y
+apt-get install x2goserver-xsession -y
+apt-get install xfce4 -y
+apt-get install iceweasel -y
+apt install xfce4-terminal -y
 sleep 1
 clear
 echo -e "\n Installation terminé, si le service X2GO n'a pas démarré taper 'service x2goserver start' \n"
